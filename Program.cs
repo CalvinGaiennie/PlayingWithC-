27a8d570511d99dc3e66fa4197b632e82373a78b
CalvinGaiennie <calvingaiennie@gmail.com>
2025-01-22 13:40:14 -0600

    started playing around with some fundamental C# concepts

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//declaring variables
+//If statements
+//loops 
+//logging
+//string formatting
+//collections (lists arrays dictionaries)
+//methods and functions
+//classes and objects
+//Linq
+// file operations like read and write
+
+
+using System;
+class Program 
+{    
+    static void Main() 
+    {
+        Console.WriteLine("Do you choose A or B?");
+        string choice = Console.ReadLine();
+        string choiceUpper = choice.ToUpper();
+
+        if ( choiceUpper == "A" || choiceUpper == "B" )
+        {
+            Console.WriteLine(choiceUpper);
+        }
+        else 
+        {
+            Console.WriteLine("invalid input");
+        }
+}
+}
