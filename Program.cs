a761d8f2077481c7e591d67ac5279c7b5ee0b627
CalvinGaiennie <calvingaiennie@gmail.com>
2025-01-28 23:04:32 -0600

    got rid of last stuff and got the read write file stuff working how i wanted. now its time to think of a project to build

--- Program.cs
+++ Program.cs
@@ -19,60 +19,16 @@
 using System;
 using System.Text.Json.Nodes;
 
-public class UserData
-{
-    public string Name { get; set; }
-    public int Age { get; set; }
-    public int Year { get; set; }
-    public string Status { get; set; }
-    public string Choice { get; set; }
-}
+
 class Program 
 {    
     static void Main() 
     {
-        Console.WriteLine("Do you choose A or B?");
-        string choice = Console.ReadLine();
-        string choiceUpper = choice.ToUpper();
-
-        Console.WriteLine("what is your name?");
-        string name = Console.ReadLine();
-        
-        if ( choiceUpper == "A" || choiceUpper == "B" )
-        {
-            Console.WriteLine(choiceUpper);
-        }
-        else 
-        {
-            Console.WriteLine("invalid input. Your answer should be 'A' or 'B'");
-        }
-        Console.WriteLine("How old are you?");
-        string ageInput = Console.ReadLine();
-        //string interpolation
-        Console.WriteLine($"You are {ageInput} years old");
-        DateTime now = DateTime.Now;
-
-        int.TryParse(ageInput, out int age);
-
-        string[] numbersAsWords = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "twenty-one", "twenty-two", "twenty-three", "twenty-four", "twenty-five", "twenty-six", "twenty-seven", "twenty-eight", "twenty-nine", "thirty", "thirty-one", "thirty-two", "thirty-three", "thirty-four", "thirty-five", "thirty-six", "thirty-seven", "thirty-eight", "thirty-nine", "forty", "forty-one", "forty-two", "forty-three", "forty-four", "forty-five", "forty-six", "forty-seven", "forty-eight", "forty-nine", "fifty", "fifty-one", "fifty-two", "fifty-three", "fifty-four", "fifty-five", "fifty-six", "fifty-seven", "fifty-eight", "fifty-nine", "sixty", "sixty-one", "sixty-two", "sixty-three", "sixty-four", "sixty-five", "sixty-six", "sixty-seven", "sixty-eight", "sixty-nine", "seventy", "seventy-one", "seventy-two", "seventy-three", "seventy-four", "seventy-five", "seventy-six", "seventy-seven", "seventy-eight", "seventy-nine", "eighty", "eighty-one", "eighty-two", "eighty-three", "eighty-four", "eighty-five", "eighty-six", "eighty-seven", "eighty-eight", "eighty-nine", "ninety", "ninety-one", "ninety-two", "ninety-three", "ninety-four", "ninety-five", "ninety-six", "ninety-seven", "ninety-eight", "ninety-nine", "one hundred"];
-        
-        for (int i = age; i >= 0; i--)
-        {
-            int year = now.Year - i; 
-            Console.WriteLine($"You were {numbersAsWords[age - i]} years old in {year}");
-        }
-        Console.WriteLine(age > 18 ? "You are an adult" : "You are a minor");
-        var userData = new {
-            Name = name,
-            Age = age,
-            Year = now.Year
-        };
-        string jsonString = System.Text.Json.JsonSerializer.Serialize(userData);
-        File.AppendAllText("userInfo.txt", jsonString);
-        string fileContent = File.ReadAllText("userInfo.txt");
-
-         UserData deserializedData = System.Text.Json.JsonSerializer.Deserialize<UserData>(fileContent);
-
-         Console.WriteLine(deserializedData);
+      Console.WriteLine("Write something");
+      string input = Console.ReadLine();
+      string formattedInput = $"{input} \n";
+      File.AppendAllText("output.txt", formattedInput);
+      string output = File.ReadAllText("output.txt");
+      Console.WriteLine(output);
     }
 }       
