da0309a4bcdb1baad933b112f8e9242b4d7b1195
CalvinGaiennie <calvingaiennie@gmail.com>
2025-07-11 18:15:10 -0500

    old commit

--- obj/Debug/net9.0/PlayingWithC#.AssemblyInfo.cs
+++ obj/Debug/net9.0/PlayingWithC#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlayingWithC#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a761d8f2077481c7e591d67ac5279c7b5ee0b627")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a94e5c193273dfdfcbb4f7487af17a875314811a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlayingWithC#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlayingWithC#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
